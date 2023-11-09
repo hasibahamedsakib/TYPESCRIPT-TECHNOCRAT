d53f6d26db0dbcff82b6b9825a9522b4be8d4379
Hasib Ahamed Sakib <hasibahamedsakib1@gmail.com>
2023-11-09 23:21:47 +0600

    compleate module-1

--- Module_01/src/1.11.ts
+++ Module_01/src/1.11.ts
@@ -2,4 +2,29 @@
   let age = 17;
   let result = age > 18 ? "Adult" : "Not Adult";
   console.log(result);
+
+  // nullish operator
+  type User = {
+    id: number;
+    name: string;
+    email?: string;
+    address: {
+      present: string;
+      permanent?: string;
+    };
+  };
+
+  const user: User = {
+    id: 101,
+    name: "hasib",
+    // email: 'h@gmail.com',
+    address: {
+      // permanent: "nothing",
+      present: "Rangpur",
+    },
+  };
+
+  const address = user?.address?.permanent ?? "No Permanent address ";
+  console.log(address);
+  console.log(user?.address?.permanent);
 }
---
+++ Module_01/src/1.12.ts
@@ -0,0 +1,15 @@
+{
+  const getMiterPerSecond = (params: unknown) => {
+    if (typeof params === "number") {
+      const convertedSpeed = (params * 1000) / 3600;
+      console.log("Converted speed is ", convertedSpeed, " km/s");
+    } else if (typeof params === "string") {
+      const [number, string] = params.split(" ");
+      const convertedSpeed = (parseFloat(number) * 1000) / 3600;
+      console.log("Converted speed is ", convertedSpeed, " km/s");
+    } else {
+      console.log("input is invalid");
+    }
+  };
+  getMiterPerSecond(120);
+}
